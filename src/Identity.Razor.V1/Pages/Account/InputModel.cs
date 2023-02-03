5503f5b7de88f4276d45e896db6ebe1d1e3fee11
Maxim Zavidei <maxim-zavidei+git@pm.me>
2023-02-03 21:17:09 +0100

    Added authentication and login pages

---
+++ src/Identity.Razor.V1/Pages/Account/InputModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Razor.V1.Pages.Account.Login;
+
+public class InputModel
+{
+    [Required]
+    [Display(Name = "User Name")]
+    public required string UserName { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+}
---
+++ src/Identity.Razor.V1/Pages/Account/LoginModel.cshtml.cs
@@ -0,0 +1,41 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Identity.Razor.V1.Pages.Account.Login;
+
+public class LoginModel : PageModel
+{
+    // Provides 2 way data binding
+    [BindProperty]
+    public required InputModel InputModel { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid) return Page();
+
+        // Verify the credentials
+        if (InputModel.UserName == "admin" && InputModel.Password == "password")
+        {
+            // Create the security context
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "admin"),
+                new Claim(ClaimTypes.Email, "admin@gmail.com")
+            };
+            var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+            // Serializes the claims principal into a string, encrypts that string and saves it as a cookie in the HttpContext
+            await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+            return RedirectToPage("/Index");
+        }
+        return Page();
+    }
+}
