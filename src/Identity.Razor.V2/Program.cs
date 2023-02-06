6cc14cb004d0b96a360bf5a57b75067ae65b1944
Maxim Zavidei <maxim-zavidei+git@pm.me>
2023-02-06 23:02:56 +0100

    Added identity settings

--- src/Identity.Razor.V2/Program.cs
+++ src/Identity.Razor.V2/Program.cs
@@ -1,4 +1,5 @@
 using Identity.Razor.V2.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(opt =>
 {
     opt.UseNpgsql(builder.Configuration["ConnectionStrings:DefaultConnection"]);
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt =>
+{
+    opt.Password.RequiredLength = 8;
+    opt.Password.RequireLowercase = true;
+    opt.Password.RequireUppercase = true;
+    opt.Lockout.MaxFailedAccessAttempts = 5;
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+    opt.User.RequireUniqueEmail = true;
+})
+// Tell the identity system to use this particular db context to store users.
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.ConfigureApplicationCookie(opt =>
+{
+    opt.LoginPath = "/Account/Login";
+    opt.AccessDeniedPath = "/Account/AccessDenied";
 });
 
 builder.Services.AddRazorPages();
@@ -25,6 +44,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
