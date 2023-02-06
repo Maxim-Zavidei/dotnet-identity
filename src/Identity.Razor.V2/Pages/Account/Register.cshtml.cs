ede219dcf9268e83ffd48f1c34e7bcbd0027a12d
Maxim Zavidei <maxim-zavidei+git@pm.me>
2023-02-07 00:38:28 +0100

    Added a registration page

---
+++ src/Identity.Razor.V2/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,65 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Identity.Razor.V2.Pages;
+
+
+public class RegisterModel : PageModel
+{
+    private readonly UserManager<IdentityUser> userManager;
+
+    [BindProperty]
+    public required RegisterViewModel RegisterViewModel { get; set; }
+
+    public RegisterModel(UserManager<IdentityUser> userManager)
+    {
+        this.userManager = userManager;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid) return Page();
+
+        // Validating email address
+
+
+        // Create the user
+        var user = new IdentityUser
+        {
+            Email = RegisterViewModel.Email,
+            UserName = RegisterViewModel.Email
+        };
+
+        var result = await this.userManager.CreateAsync(user, RegisterViewModel.Password);
+        if (result.Succeeded)
+        {
+            return RedirectToPage("/Account/Login");
+        }
+        else
+        {
+            foreach(var error in result.Errors)
+            {
+                ModelState.AddModelError("Register", error.Description);
+            }
+
+            return Page();
+        }
+    }
+}
+
+public class RegisterViewModel
+{
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    public required string Email { get; set; }
+
+    [Required]
+    [DataType(dataType: DataType.Password)]
+    public required string Password { get; set; }
+}
