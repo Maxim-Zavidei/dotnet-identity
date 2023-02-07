6c741fd09c43bb1e3e5993fb9e0bc281f7d6f8ec
Maxim Zavidei <maxim-zavidei+git@pm.me>
2023-02-07 21:48:58 +0100

    Added a authentication partial

--- src/Identity.Razor.V2/Program.cs
+++ src/Identity.Razor.V2/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt =>
     opt.Lockout.MaxFailedAccessAttempts = 5;
     opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
     opt.User.RequireUniqueEmail = true;
-    opt.SignIn.RequireConfirmedEmail = true;
+    opt.SignIn.RequireConfirmedEmail = false;
 })
 // Tell the identity system to use this particular db context to store users.
 .AddEntityFrameworkStores<ApplicationDbContext>()
